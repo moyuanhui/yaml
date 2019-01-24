fb63270abf6add5090f5de479205c56dc2cddaac
莫元辉 <moyhui@hengkangit.com>
2019-01-24 14:20:05 +0800

    apollo

--- apollo/hack/run.sh
+++ apollo/hack/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-helm install --name apollo --namespace wrd-infra ./ \
+helm install --name apollo --namespace apollo ./ \
              --set uatconfigservice.extraEnv."eureka\.instance\.hostname"="apollo-uatconfigservice" \
              --set uatconfigservice.extraEnv."eureka\.instance\.preferIpAddress"=\"false\" \
              --set uatadminservice.extraEnv."eureka\.instance\.hostname"="apollo-uatadminservice" \
@@ -10,7 +10,3 @@ helm install --name apollo --namespace wrd-infra ./ \
              --set proconfigservice.extraEnv."eureka\.instance\.preferIpAddress"=\"false\" \
              --set proadminservice.extraEnv."eureka\.instance\.hostname"="apollo-proadminservice" \
              --set proadminservice.extraEnv."eureka\.instance\.preferIpAddress"=\"false\"
-             \
-             --set portalmysql.persistence.storageClass="local-storage" \
-             --set proconfigservicemysql.persistence.storageClass="local-storage" \
-             --set uatconfigservicemysql.persistence.storageClass="local-storage"
